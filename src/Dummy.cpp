da55402c12f2419938144cfe64e2758c03722060
Me Dumb <me@home.org>
2017-03-10 14:06:58 +0100

    Initial commit

---
+++ src/Dummy.cpp
@@ -0,0 +1,10 @@
+#include "Dummy.hpp"
+#include <iostream>
+
+using namespace std;
+using namespace dsp1760;
+
+void DummyClass::welcome()
+{
+    cout << "You successfully compiled and executed DummyProject. Welcome!" << endl;
+}
---
+++ src/Main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <dsp1760/Dummy.hpp>
+
+int main(int argc, char** argv)
+{
+    dsp1760::DummyClass dummyClass;
+    dummyClass.welcome();
+
+    return 0;
+}
